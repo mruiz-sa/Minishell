abef3dd61b2ec6f04b3b9135e1d9332c62bf08cf
manu-garcia <garcia.martin.manu@gmail.com>
2022-09-07 22:10:01 +0200

    Adds data structs and pseudo code

--- include/minishell.h
+++ include/minishell.h
@@ -3,17 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mruiz-sa <mruiz-sa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manu <manu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 12:23:43 by mruiz-sa          #+#    #+#             */
-/*   Updated: 2022/09/06 18:35:28 by mruiz-sa         ###   ########.fr       */
+/*   Updated: 2022/09/07 21:59:45 by manu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-typedef struct s_prompt{
+typedef enum e_token_type {
+	NONE		= 0,
+	CMD			= 1,
+	ARG			= 2,
+	GREAT		= 3,
+	GREATGREAT	= 4,
+	LESS		= 5,
+	PIPE		= 6,
+	AMP			= 7,
+}	t_token_type;
+
+typedef struct s_token {
+	t_token_type	type;
+}	t_token;
+
+typedef struct s_simple_cmd {
+	int		argc;
+	char	**argv;
+}	t_simple_cmd;
+
+typedef struct s_cmd {
+	int				ncmds;
+	t_simple_cmd	**cmds;
+	void			*outFile;
+	void			*inFile;
+	void			*errFile;
+	int				background;
+}	t_cmd;
+
+typedef struct s_prompt {
 	char	*user;
 	char	*prompt;
 	char	*pwd;
--- src/ft_read.c
+++ src/ft_read.c
@@ -6,7 +6,7 @@
 /*   By: manu <manu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 13:14:19 by mruiz-sa          #+#    #+#             */
-/*   Updated: 2022/09/07 20:03:58 by manu             ###   ########.fr       */
+/*   Updated: 2022/09/07 22:09:07 by manu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,15 @@ char	*ft_read(t_mini *state)
 	// if (ft_strlen(state->readline))
 	// 	add_history(state->readline);
 	ft_lexer(state->readline, ' ');
+	// lexer converts state->readline into tokens (t_token in minishell.h)
+	// parser validate grammar, converts tokens into a command table (t_cmd in minishell.h)
+	// convert state->readline into a command table
+	// 		break state->readline into simple commands (t_simple_cmd in minishell.h)
+	//		simple commands are separated by pipes
+	//		per each command, expand variables and wildcards
+	// execute the command table
+	// 		for each command create a new process
+	//		wait process. In "ls -a | wc", "wc" process should wait for "ls" to finish?
 	free_prompt(&state->prompt);
 	if (!ft_strncmp(state->readline, "exit", 4))
 		exit_without_error(state);
